757f3768e1cc14f4b579ab5b7289116a31e2719b
Maitreyee <86715433+maitreyee-xo@users.noreply.github.com>
2022-06-06 14:22:37 +0530

    Create score_board.py

---
+++ score_board.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+class ScoreBoard(Turtle):
+
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-350,290)
+        self.write(self.l_score, font = ("Courier", 26, "bold"))
+        self.goto(350,290)
+        self.write(self.r_score, font = ("Courier", 26, "bold"))
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
