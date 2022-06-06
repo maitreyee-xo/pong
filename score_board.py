00df121ae8e3bd1cff9601be9915adfe6e200c3a
Maitreyee Singh <86715433+maitreyee-xo@users.noreply.github.com>
2022-06-06 20:26:09 +0530

    Update score_board.py

--- score_board.py
+++ score_board.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 
-class ScoreBoard(Turtle):
 
+class ScoreBoard(Turtle):
 
     def __init__(self):
         super().__init__()
@@ -11,12 +11,13 @@ class ScoreBoard(Turtle):
         self.l_score = 0
         self.r_score = 0
         self.update_scoreboard()
+
     def update_scoreboard(self):
         self.clear()
-        self.goto(-350,290)
-        self.write(self.l_score, font = ("Courier", 26, "bold"))
-        self.goto(350,290)
-        self.write(self.r_score, font = ("Courier", 26, "bold"))
+        self.goto(-350, 290)
+        self.write(self.l_score, font=("Courier", 26, "bold"))
+        self.goto(350, 290)
+        self.write(self.r_score, font=("Courier", 26, "bold"))
 
     def l_point(self):
         self.l_score += 1
